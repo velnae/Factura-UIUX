4d8347c8c418f841258f4901f86a4c34f81350fb
Velnae <nahuinlla101131@gmail.com>
2023-01-23 17:25:34 -0500

    Sincronizacion de tabla producto

--- js/main-jhon.js
+++ js/main-jhon.js
@@ -15,6 +15,7 @@ arrayProducts = [
 var selectProducts = document.getElementById('selectProducts');
 var btnAddTable = document.getElementById('btnAddTable');
 var tableProducts = document.getElementById('tbl-productos');
+var tableProductsTxt = document.getElementById('tbl-productos-txt');
 
 arrayProducts.forEach(function(product) {
     var option = document.createElement('option');
@@ -40,18 +41,39 @@ btnAddTable.addEventListener('click', function() {
     products.forEach(function(product) {
         // insertar fila en el tbody
         var tbody = tableProducts.querySelector('tbody');
+        var tbodyTxt = tableProductsTxt.querySelector('tbody');
+
         var row = tbody.insertRow();
+        var rowTxt = tbodyTxt.insertRow();
+
         row.insertCell().innerHTML = "<input id='cantidad' type='number' class='form-control' value='1' style='text-align: right'>"
+        rowTxt.insertCell().innerHTML = '1';
+
         row.insertCell().innerHTML = product.name;
+        rowTxt.insertCell().innerHTML = product.name;
+
         row.insertCell().innerHTML = product.priceUnit;
+        rowTxt.insertCell().innerHTML = product.priceUnit;
+
         var cantidad = row.querySelector('#cantidad').value;
         var precieTotal = product.priceUnit * cantidad;
+
         row.insertCell().innerHTML = precieTotal;
+        rowTxt.insertCell().innerHTML = precieTotal;
+
+        $(rowTxt).effect("highlight", {color: 'yellow'}, 1000);
+
         // si cambia la cantidad
         row.querySelector('#cantidad').addEventListener('change', function() {
             var cantidad = row.querySelector('#cantidad').value;
             var precieTotal = product.priceUnit * cantidad;
+
             row.querySelector('td:nth-child(4)').innerHTML = precieTotal;
+            rowTxt.querySelector('td:nth-child(1)').innerHTML = cantidad;
+            rowTxt.querySelector('td:nth-child(4)').innerHTML = precieTotal;
+
+            $(rowTxt).find('td:nth-child(1)').effect("highlight", {color: 'yellow'}, 1000);
+            $(rowTxt).find('td:nth-child(4)').effect("highlight", {color: 'yellow'}, 1000);
 
             // calculando el total y tambien el total 
             calcularTotal();
@@ -65,7 +87,8 @@ btnAddTable.addEventListener('click', function() {
         // agregar evento al boton eliminar
         cell.querySelector('button').addEventListener('click', function() {
             row.remove();
-            // calculando el total y tambien el total 
+            rowTxt.remove();
+            // calculando el total y tambien el total
             calcularTotal();
         });
     });
@@ -89,8 +112,14 @@ function calcularTotal() {
     // calculando el total y tambien el total 
     let subTotal = 0;
     let $subTotal = document.getElementById('subTotal');
+    let $txtSubTotal = document.getElementById('txtSubtotal');
+
     let $igv = document.getElementById('igv');
+    let $txtIGV = document.getElementById('txtIGV');
+
     let $precioTotal = document.getElementById('precioTotal');
+    let $txtTotal = document.getElementById('txtTotal');
+
     let total = 0;
     let $tds = tableProducts.querySelectorAll('td:nth-child(4)');
 
@@ -100,10 +129,16 @@ function calcularTotal() {
 
     subTotal = (total / 1.18).toFixed(2);
     $subTotal.innerHTML = subTotal;
+    $txtSubTotal.innerHTML = subTotal;
+    $($txtSubTotal).effect("highlight", {color: 'yellow'}, 1000);
 
     $igv.innerHTML = (total - subTotal).toFixed(2);
+    $txtIGV.innerHTML = (total - subTotal).toFixed(2);
+    $($txtIGV).effect("highlight", {color: 'yellow'}, 1000);;
 
     $precioTotal.innerHTML = total;
+    $txtTotal.innerHTML = total;
+    $($txtTotal).effect("highlight", {color: 'yellow'}, 1000);
 
     let  $quatityProducts = document.getElementById('quatityProducts');
     $quatityProducts.innerHTML = $tds.length;
